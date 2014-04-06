891f3c79aead2c19722de44ed91a9f4115c26986
Gary Burgess <gary.burgess@gmail.com>
2014-04-07 00:14:03 +0100

    Add applicative instance

--- src/Control/Monad/Free.purs.hs
+++ src/Control/Monad/Free.purs.hs
@@ -12,6 +12,10 @@ instance functorFree :: (Functor f) => Functor (Free f) where
   (<$>) f = go where
     go (Pure a)  = Pure (f a)
     go (Free fa) = Free (go <$> fa)
+    
+instance applicativeFree :: (Functor f) => Applicative (Free f) where
+  pure = return
+  (<*>) = ap
 
 instance monadFree :: (Functor f) => Monad (Free f) where
   return = Pure
