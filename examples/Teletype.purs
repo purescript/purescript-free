da17162020ed9b96cce6775d9e4d8dcd68afef19
eric thul <thul.eric@gmail.com>
2014-05-27 21:01:09 -0400

    Updating example to use iterM

--- examples/Teletype.purs
+++ examples/Teletype.purs
@@ -18,12 +18,13 @@ putStrLn s = liftF $ PutStrLn s {}
 getLine :: Teletype String
 getLine = liftF $ GetLine (\a -> a)
 
-run :: forall e a. Teletype a -> Eff (trace :: Trace | e) a
-run (Pure a) = return a
-run (Free (PutStrLn s a)) = trace s >>= (\_ -> run a)
-run (Free (GetLine k)) = run $ k "fake input"
+runF :: forall a. TeletypeF (Eff (trace :: Trace) a) -> Eff (trace :: Trace) a
+runF (PutStrLn s a) = trace s >>= (\_ -> a)
+runF (GetLine k) = k "fake input"
 
-echo :: Teletype {}
+run :: forall a. Teletype a -> Eff (trace :: Trace) a
+run = iterM runF
+
 echo = do
   a <- getLine
   putStrLn a
