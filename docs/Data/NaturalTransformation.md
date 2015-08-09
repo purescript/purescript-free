4bf96226beec6f9a0437d64e931f6a88a520a733
eric thul <thul.eric@gmail.com>
2015-08-08 22:37:29 -0400

    Adding Natural alias for NaturalTransformation

--- docs/Data/NaturalTransformation.md
+++ docs/Data/NaturalTransformation.md
@@ -8,4 +8,12 @@ type NaturalTransformation f g = forall a. f a -> g a
 
 A natural transformation
 
+#### `Natural`
+
+``` purescript
+type Natural f g = NaturalTransformation f g
+```
+
+Alias for `NaturalTransformation`
+
 
--- src/Data/NaturalTransformation.purs
+++ src/Data/NaturalTransformation.purs
@@ -1,4 +1,10 @@
-module Data.NaturalTransformation (NaturalTransformation(..)) where
+module Data.NaturalTransformation
+  ( NaturalTransformation(..)
+  , Natural(..)
+  ) where
 
 -- | A natural transformation
 type NaturalTransformation f g = forall a. f a -> g a
+
+-- | Alias for `NaturalTransformation`
+type Natural f g = NaturalTransformation f g
