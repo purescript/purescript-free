830d139aa29f5396a448724da0c57573f255ee92
Irakli Safareli <i.safareli@gmail.com>
2017-04-24 21:46:02 +0400

    use ((->) Unit) instead of Lazy

--- src/Control/Monad/Trampoline.purs
+++ src/Control/Monad/Trampoline.purs
@@ -5,40 +5,29 @@
 module Control.Monad.Trampoline
   ( Trampoline
   , done
-  , suspend
-  , delay'
   , delay
   , runTrampoline
   ) where
 
 import Prelude
 
-import Control.Monad.Free (Free, liftF, runFree, suspendF)
+import Control.Monad.Free (Free, liftF, runFree)
 
-import Data.Lazy (Lazy, force, defer)
 
 -- | The `Trampoline` monad
 -- |
 -- | A computation of type `Trampoline a` consists of zero or more lazy
 -- | suspensions before a value is returned.
-type Trampoline = Free Lazy
+type Trampoline = Free ((->) Unit)
 
 -- | Return a value immediately
 done :: forall a. a -> Trampoline a
 done = pure
 
--- | Suspend a computation by one step.
-suspend :: forall a. Trampoline a -> Trampoline a
-suspend = suspendF
-
--- | Use the `Trampoline` monad to represent a `Lazy` value.
-delay' :: forall a. Lazy a -> Trampoline a
-delay' = liftF
-
 -- | Use the `Trampoline` monad to represent the delayed evaluation of a value.
 delay :: forall a. (Unit -> a) -> Trampoline a
-delay = delay' <<< defer
+delay = liftF
 
 -- | Run a computation in the `Trampoline` monad.
 runTrampoline :: forall a. Trampoline a -> a
-runTrampoline = runFree force
+runTrampoline = runFree (_ $ unit)
